4a223652fb22e1e04a4309dddd75d720eded765d
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-05 13:42:27 -0700

    Splitting of strings

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = print(word[0:2])
+word_last_2 = print(word[7:9])
+middle_word = print(word[3:5])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
