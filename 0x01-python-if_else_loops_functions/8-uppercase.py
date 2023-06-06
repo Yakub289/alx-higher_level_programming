7857e846d0672fa88bea0f2dc0977de1fdd7b7fa
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-06 14:33:15 -0700

    Uppercase character with function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -8,5 +8,5 @@ def uppercase(str):
 
         if ord(uppercase) >= 97 and ord(uppercase) <= 122:
             uppercase = chr(ord(uppercase) - 32)
-            print("{:c}".format(uppercase), end="")
+            print("{}".format(uppercase), end="")
             print("")
