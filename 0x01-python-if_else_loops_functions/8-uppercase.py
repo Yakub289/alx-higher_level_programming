84066911d99f7e896f09590ca82eca9003981850
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-06 19:29:42 -0700

    Testing

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Displaying alphabet in change case.
 # Using a function that checks for character case.
 
-def uppercase(str):
+uppercase = __import__('8-uppercase').uppercase
 
-    for uppercase in range(65, 90):
-        if ord(uppercase) <= 97 and ord(uppercase) >= 122:
-            uppercase = chr(ord(uppercase) - 32)
-        print("{}".format(uppercase), end="")
-    print("Z")
+uppercase("best")
+uppercase("Best School 98 Battery Street")
