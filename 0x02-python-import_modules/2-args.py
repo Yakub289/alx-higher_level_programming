35801569c69cc9facdf6681d86ef9bf0ddc779f3
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-08 08:29:34 -0700

    List nad show length

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 from sys import argv
-
 def length():
-
     print('{} argument'.format(len(argv) - 1), end='')
-
     if len(argv) == 1:
         print('s.')
     elif len(argv) == 2:
@@ -14,6 +10,5 @@ def length():
         print('s:')
     for axe in range(1, len(argv)):
         print('{}: {}'.format(axe, argv[axe]))
-
 if __name__ == "__main__":
     length()
