b7f43879f2383bdb5af53cb5d829ee5f4cc9704d
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-08 05:58:36 -0700

    Importation of function from file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,11 +3,10 @@
 # Write a program that imports the function def add(a, b):
 # from the file add_0.py and prints the result of the addition 1 + 2 = 3.
 
-def add(a,b):
+if __name__ == " __main__":
 
     from add_0 import add
     a = 1
     b = 2
     result = a + b
     print("<a value> + <b value> = <add(a, b) value>: ", "{:d}".format(result))
-    add("a,b", "result")
