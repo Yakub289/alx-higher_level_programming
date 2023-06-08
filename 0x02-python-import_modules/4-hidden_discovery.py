9b4f701a93c9bcd82a50166c72ad79fe80e1209a
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-08 08:59:26 -0700

    Hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+def principal():
+    for sequences in dir(hidden_4):
+        if not (sequences[0] == '_' and sequences[1] == '_'):
+            print(sequences)
+
+if __name__ == "__main__":
+    principal()
