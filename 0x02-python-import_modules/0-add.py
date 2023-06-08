009360471ff3456bdf82a13caaa835ca6583e604
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-08 06:52:51 -0700

    Addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-# Write a program that imports the function def add(a, b):
-# add_0.py and prints the result of the addition 1 + 2 = 3.
+# Write a program that imports the function def add(a,b)
 
 if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("<a value> + <b value> = <add(a, b) value>: ", "{:d}".format(result))
