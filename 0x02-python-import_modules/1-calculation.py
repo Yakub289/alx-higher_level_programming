4e8d2b4eb49cd312cef6d6aa692191f471bc3723
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-08 07:37:40 -0700

    Mathematics

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,12 +3,9 @@
 # Imports functions from the file calculator_1.py
 
 if __name__ == "__main__":
-
     from calculator_1 import add, div, mul, sub
-
     a = 10
     b = 5
-
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
