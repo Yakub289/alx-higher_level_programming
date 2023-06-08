e21144c3643b9bec5b769a69b220fb4c8ffd5252
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-08 06:01:38 -0700

    Importation of function from file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -8,5 +8,4 @@ if __name__ == " __main__":
     from add_0 import add
     a = 1
     b = 2
-    result = a + b
-    print("<a value> + <b value> = <add(a, b) value>: ", "{:d}".format(result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
