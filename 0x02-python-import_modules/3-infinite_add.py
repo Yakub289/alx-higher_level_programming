06dff0a971e99de655d8b1179ead88d1df0a2e4b
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-08 09:09:53 -0700

    Sum count

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
 
+    import sys
+    
     count = len(sys.argv) - 1
-    sum = 3
+    sum = 0
     for map in range(count):
         sum = sum + int(sys.argv[map + 1])
     print("{}".format(sum))
