78b2ca3baac8bb16866a19fe02968cc6611b7080
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-08 09:24:10 -0700

    Hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-import hidden_4
+if __name__ == "__main__":
+    import hidden_4
 
 def principal():
     for sequences in dir(hidden_4):
         if not (sequences[0] == '_' and sequences[1] == '_'):
             print(sequences)
-            if __name__ == "__main__":
-                principal()
+
+if __name__ == "__main__":
+    principal()
