eac67966d8294ae78ea9ca238b6f8ecde35f07ec
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-10 06:16:03 -0700

    Case check

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Write a function that returns a tuple with
+#  the length of a string and its first character.
+
+def multiple_returns(sentence):
+    if sentence != "":
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (len(sentence), first_char)
