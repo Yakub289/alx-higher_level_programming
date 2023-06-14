240522890590e1e4c216127561fd3a829d51f382
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-14 06:30:57 -0700

    The score-best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,10 +5,10 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return (None)
-    dict = 0
+    set = 0
     for list in a_dictionary:
-        if dict < a_dictionary[list]:
-            dict = a_dictionary[list]
+        if set < a_dictionary[list]:
+            set = a_dictionary[list]
     for list in a_dictionary.keys():
-        if a_dictionary[list] == dict:
+        if a_dictionary[list] == set:
             return (list)
