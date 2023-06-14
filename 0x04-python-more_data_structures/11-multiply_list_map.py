f8e2d3e3437bb6fd29435857bfb34231a5e2c05c
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-14 06:37:57 -0700

    Multiply by using map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -4,4 +4,4 @@
 # multiplied by a number without using any loops.
 
 def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda items: items * number, my_list))
+    return list(map(lambda s: s * number, my_list))
