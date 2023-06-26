47c683e8b2937a94cbdedff12bfa494e4f117a56
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-26 05:46:01 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,15 +9,12 @@
 # You have to use try: / except:
 
 def safe_print_list(my_list=[], x=0):
-    digit = 0
-    if isinstance(my_list, list) and isinstance(x, int):
-        for numbers in range(x):
-            try:
-                print("{}".format(my_list[numbers]), end="")
-            except IndexError:
-                pass
-            else:
-                digit += 1
-                print("")
-
-                return (digit)
+    number = 0
+    for digits in range(x):
+        try:
+            print(f"{my_list[digits]}", end="")
+            number += 1
+        except IndexError:
+            break
+    print("")
+    return(number)
