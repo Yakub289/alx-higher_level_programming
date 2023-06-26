a76d07b1cfb28dc54f4084b2d349f1cd21f384ed
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-26 05:36:03 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -13,10 +13,11 @@ def safe_print_list(my_list=[], x=0):
     if isinstance(my_list, list) and isinstance(x, int):
         for numbers in range(x):
             try:
-                print("{}".format(my_list[numbers]), end="\n")
+                print("{}".format(my_list[numbers]), end="")
             except IndexError:
                 pass
             else:
                 digit += 1
-    print("\n")
-    return (digit)
+                print("")
+
+                return (digit)
