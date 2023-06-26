81b31da99e4daebb24019d9d26c34293138e7026
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-26 11:21:33 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,16 +3,12 @@
 # Write a function that prints x elements of a list.
 # my_list can contain any type (integer, string, etc.)
 # All elements must be printed on the same line followed by a new line.
-# x represents the number of elements to print
-# x can be bigger than the length of my_list
-# Returns the real number of elements printed
-# You have to use try: / except:
 
 def safe_print_list(my_list=[], x=0):
     number = 0
     for digits in range(x):
         try:
-            print(f"{my_list[digits]}", end="\n")
+            print(f"{my_list[digits]}", end="")
             number += 1
         except IndexError:
             break
