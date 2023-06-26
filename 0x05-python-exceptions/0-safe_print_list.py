6021e7f00054c40f83095ca1c76ff639a2fa9536
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-26 11:23:14 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 # Write a function that prints x elements of a list.
-# my_list can contain any type (integer, string, etc.)
-# All elements must be printed on the same line followed by a new line.
 
 def safe_print_list(my_list=[], x=0):
     number = 0
