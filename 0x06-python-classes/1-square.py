f52e742e798cd1fff2a8b2564b73883c9beee176
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-27 05:01:06 -0700

    Define Square with size under private

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-# Write a class Square that defines a square by: (based on 0-square.py)
+"""Write a class Square that defines a square by: (based on 0-square.py)"""
+
 
 class Square:
+    """define and initilized square"""
     def __init__(self, size):
+        """Assign key"""
         self.size = size
