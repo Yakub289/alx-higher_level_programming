ddaffb2110977cc02b802652ecae9ff3b1e3d28e
Yakub289 <Yakubsuleiman24@gmail.com>
2023-06-27 05:50:39 -0700

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# Write a class Square that defines a square by: (based on 1-square.py)
+
+
+class Square:
+    """define and initilized square"""
+    def __init__(self, size):
+        """Assign key"""
+        self.__size = size
+        """ileterate conditionl statement"""
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                raise TypeError("size must be an integer")
