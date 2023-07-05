46675d5ef3e925691007690d909d358e8b7947b0
Yakub289 <Yakubsuleiman24@gmail.com>
2023-07-05 12:52:42 -0700

    Matrix multiplication

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -13,9 +13,9 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list of lists")
     if not(all(type(row) == list for row in m_b)):
         raise TypeError("m_b must be a list of lists")
-    if m_a == [] or m_a[0] == []:
+    if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b[0] == []:
+    if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
     if not(all(type(e) in [float, int] for e in [e for row in m_a for e in
                                                  row])):
