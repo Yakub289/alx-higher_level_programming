3f24f01fe92a7912140fc617747d39406fca3827
Yakub289 <Yakubsuleiman24@gmail.com>
2023-07-04 19:07:32 -0700

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    iterate_magic_string = getattr(magic_string, 'count', 0) + 1
-    return ", ".join(["BestSchool" for n in range(iterate_magic_string)])
+def magic_string(n=[]):
+    n += ["BestSchool"]
+    return (", ".join(n))
