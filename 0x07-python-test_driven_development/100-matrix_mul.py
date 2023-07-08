6dfbaf60de933794d5cc6e1f9e9bc8c6f2a68df6
Yakub289 <Yakubsuleiman24@gmail.com>
2023-07-08 11:31:59 -0700

    Matrix multiplication

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -9,23 +9,23 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
-    if not(all(type(row) == list for row in m_a)):
+    if not (all(type(row) == list for row in m_a)):
         raise TypeError("m_a must be a list of lists")
-    if not(all(type(row) == list for row in m_b)):
+    if not (all(type(row) == list for row in m_b)):
         raise TypeError("m_b must be a list of lists")
     if m_a == [] or m_a[0] == []:
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b[0] == []:
         raise ValueError("m_b can't be empty")
-    if not(all(type(e) in [float, int] for e in [e for row in m_a for e in
-                                                 row])):
+    if not (all(type(e) in [float, int] for e in [e for row in m_a for e in
+                                                  row])):
         raise TypeError("m_a should contain only integers or floats")
-    if not(all(type(e) in [float, int] for e in [e for row in m_b for e in
-                                                 row])):
+    if not (all(type(e) in [float, int] for e in [e for row in m_b for e in
+                                                  row])):
         raise TypeError("m_b should contain only integers or floats")
-    if not(all(len(row) == len(m_a[0]) for row in m_a)):
+    if not (all(len(row) == len(m_a[0]) for row in m_a)):
         raise TypeError("each row of m_a must be of the same size")
-    if not(all(len(row) == len(m_b[0]) for row in m_b)):
+    if not (all(len(row) == len(m_b[0]) for row in m_b)):
         raise TypeError("each row of m_b must be of the same size")
 
     score = len(m_a[0])
