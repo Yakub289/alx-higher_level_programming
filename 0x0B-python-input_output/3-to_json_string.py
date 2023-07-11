a819f26f140174a68dd08b4becac8c7e64bdffac
Yakub289 <Yakubsuleiman24@gmail.com>
2023-07-11 07:27:36 -0700

    JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""Write a function that returns the JSON
+representation of an object (string)"""
+
+
+def to_json_string(my_obj):
+    """define json string and return its value"""
+    return (json.dumps(my_obj))
