96511a8f96a74d50582bde16ca17396bcbed29dd
Yakub289 <Yakubsuleiman24@gmail.com>
2023-07-11 11:29:06 -0700

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""Write a class Student that defines a student by: (based on 9-student.py)"""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """define and initialize each personeel attribute"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """define json and return its value"""
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            return ({key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)})
+
+            return (self.__dict__)
