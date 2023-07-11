5464f764d67885c807f8eaf79510927f85a449e0
Yakub289 <Yakubsuleiman24@gmail.com>
2023-07-11 06:37:35 -0700

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""Write a function that writes a string to a text file (UTF8)
+and returns the number of characters written:"""
+
+
+def number_of_lines(filename=""):
+    """Write a function that returns the number of lines of a text file"""
+    line_number = 0
+    with open(filename, 'r') as file:
+        for line in file:
+            line_number += 1
+
+    return(line_number)
