7db05831bf4808a2bb0a9a20e192b085b009bcf2
Yakub289 <Yakubsuleiman24@gmail.com>
2023-08-16 07:35:38 -0700

    Genre ID by shows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- Import the database dump of hbtn_0d_tvshows to your MySQL server:
--- download (same as 10-genre_id_by_show.sql)
 
 -- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
 
