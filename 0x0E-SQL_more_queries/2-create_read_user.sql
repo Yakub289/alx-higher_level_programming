a87866b936c94de9a88380b519d1706c5b310305
Yakub289 <Yakubsuleiman24@gmail.com>
2023-08-16 06:06:50 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -8,3 +8,4 @@
 CREATE SCHEMA IF NOT EXISTS `hbtn_0c_0`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
