6aa10bfe6333b1659f4ff192a1094188ffd55a78
Yakub289 <Yakubsuleiman24@gmail.com>
2023-08-16 06:03:55 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,5 +6,5 @@
 -- If the user user_0d_2 already exists, your script should not fail.
 
 CREATE SCHEMA IF NOT EXISTS `hbtn_0c_0`;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
