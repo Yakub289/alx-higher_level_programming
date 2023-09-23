e540951f7225fcb94fa4e0755fc168e9b1f6a498
Yakub289 <Yakubsuleiman24@gmail.com>
2023-09-22 17:31:38 -0700

    Advanced

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Write a script that lists all City objects from the database hbtn_0e_101_usa"""
+"""Write a script that lists all City objects
+from the database hbtn_0e_101_usa"""
 
 
 import sys
